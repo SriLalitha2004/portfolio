f562d072551efea139729ccbe0d10babb40b7f13
SriLalitha2004 <srilalitha.komma@gmail.com>
2025-02-14 11:29:45 +0530

    commit

--- react-portfolio/src/Footer.jsx
+++ react-portfolio/src/Footer.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FaGithub, FaLinkedin, FaTwitter, FaInstagram } from 'react-icons/fa';
+import { FaGithub, FaLinkedin,  FaInstagram } from 'react-icons/fa';
 import { motion } from 'framer-motion';
 
 const Footer = () => {
